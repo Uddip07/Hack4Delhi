0343502212cea303c9999997c5bb26c4d54d7f85
Manav Goel <goel.manav07@gmail.com>
2026-01-10 20:02:53 +0530

    fixing deployement

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,19 +3,16 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
-  base: process.env.NODE_ENV === "production" ? "/Hack4Delhi/" : "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/Hack4Delhi/" : "/",
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
-    port: "4028",
-    host: "0.0.0.0",
+    port: 4028,
+    host: "localhost",
     strictPort: true,
   }
-});
+}));
