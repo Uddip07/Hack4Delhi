0615f63f6eb04182227cf2d03d16534640a18600
Manav Goel <goel.manav07@gmail.com>
2026-01-10 19:07:21 +0530

    fixing deployement

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,24 +3,19 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Hack4Delhi/",
-  plugins: [react()]
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  base: process.env.NODE_ENV === "production" ? "/Hack4Delhi/" : "/",
+  build: {
+    outDir: "build",
+    chunkSizeWarningLimit: 2000,
+  },
+  plugins: [tsconfigPaths(), react(), tagger()],
+  server: {
+    port: "4028",
+    host: "0.0.0.0",
+    strictPort: true,
+  }
 });
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   // This changes the out put dir from dist to build
-//   // comment this out if that isn't relevant for your project
-//   build: {
-//     outDir: "build",
-//     chunkSizeWarningLimit: 2000,
-//   },
-//   plugins: [tsconfigPaths(), react(), tagger()],
-//   server: {
-//     port: "4028",
-//     host: "0.0.0.0",
-//     strictPort: true,
-//     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
-//   }
-// });
